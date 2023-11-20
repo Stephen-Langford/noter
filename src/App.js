32c6b70ad8ed429f8d39d414f731d2f6c299728c
Stephen-Langford <slangford1050@gmail.com>
2023-11-19 21:42:30 -0500

    added bpm and note length. Spaces matter.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import * as Tone from 'tone';
 
 function App() {
   const [text, setText] = useState('');
   const [instrumentType, setInstrumentType] = useState('sine');
-  const synth = new Tone.Synth().toDestination();
+  const [beatsPerMeasure, setBeatsPerMeasure] = useState(4);
+  const [beatNoteValue, setBeatNoteValue] = useState(4);
+  
+  const [synth, setSynth] = useState(new Tone.Synth().toDestination());
 
+  useEffect(() => {
+    const newSynth = new Tone.Synth({
+      oscillator: { type: instrumentType }
+    }).toDestination();
+    setSynth(newSynth);
+  }, [instrumentType]);
+  
   const handleTextChange = (event) => {
     setText(event.target.value);
   };
 
-  const playNote = (note, duration = '8n', time = Tone.now()) => {
-    synth.oscillator.type = instrumentType;
-    synth.triggerAttackRelease(note, duration, time);
+  const playNote = (note, duration = '8n', time) => {
+    Tone.Transport.scheduleOnce((time) => {
+        synth.triggerAttackRelease(note, duration, time);
+    }, time);
   };
 
   const handleSubmit = () => {
-    Tone.start();
-
-    let time = Tone.now();
+    Tone.Transport.cancel(); // Clears existing events
+    let startTime = Tone.now();
+  
     const textUpperCase = text.toUpperCase();
-
+    const duration = `${beatNoteValue}n`; 
+    let time = 0; // Start time for the first note
+  
     for (const char of textUpperCase) {
-      const note = char + '4';
-      if (Tone.Frequency(note).toFrequency()) {
-        playNote(note, '8n', time);
-        time += Tone.Time('8n').toSeconds();
-      }
+        const note = char + '4'; 
+        if (Tone.Frequency(note).toFrequency()) {
+            playNote(note, duration, `+${time}`);
+            time += Tone.Time(duration).toSeconds();
+        }
     }
+
+    Tone.Transport.start(startTime);
+  };
+
+  
+  const handleStop = () => {
+    Tone.Transport.cancel(); // Clears scheduled events
+    Tone.Transport.stop();   // Stops the Transport if it's running
   };
 
   return (
@@ -47,7 +68,36 @@ function App() {
             <option value="triangle">Triangle Wave</option>
           </select>
         </div>
-        <button onClick={handleSubmit}>Submit</button>
+        <div>
+          <label htmlFor="beats-per-measure-select">Beats per Measure: </label>
+            <select 
+              id="beats-per-measure-select" 
+              value={beatsPerMeasure} 
+              onChange={(e) => setBeatsPerMeasure(e.target.value)}
+            >
+            <option value="2">2 Beats</option>
+            <option value="3">3 Beats</option>
+            <option value="4">4 Beats</option>
+          </select>
+        </div>
+
+        <div>
+          <label htmlFor="beat-note-value-select">Note Value per Beat: </label>
+          <select 
+            id="beat-note-value-select" 
+            value={beatNoteValue} 
+            onChange={(e) => setBeatNoteValue(e.target.value)}
+          >
+            <option value="1">Whole Note (1)</option>
+            <option value="2">Half Note (2)</option>
+            <option value="4">Quarter Note (4)</option>
+            <option value="8">Eighth Note (8)</option>
+          </select>
+        </div>
+
+        <button onClick={handleSubmit}>Play</button>
+        <button onClick={handleStop}>Stop</button>
+
       </main>
       <footer>
         <a target="_blank" href="https://icons8.com/icon/12783/music">Music</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
